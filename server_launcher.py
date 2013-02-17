8031ecbc59512a06843bea8c2caaaa5f0bb094e4
Cédric Foucault <cedric.foucault@gmail.com>
2013-02-17 20:39:36 +0100

    Put server ip in argv

--- server_launcher.py
+++ server_launcher.py
@@ -1,11 +1,13 @@
 from server import *
+import sys
 
 def main():
     """Main server process"""
     PORT = 42042 # arbitrary port number to connect on for the chat
     LOCALHOST = '127.0.0.1' # ip adress of localhost
     # ip = LOCALHOST
-    ip = '192.168.1.4'
+    # ip = '192.168.1.4'
+    ip = sys.argv[1]
     server = LobbyServer((ip, PORT))
     # server = IngamePartyServer((LOCALHOST, PORT))
     threading.Thread(target=server.serve_forever).start()
